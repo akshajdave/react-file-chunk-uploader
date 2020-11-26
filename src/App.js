9d1fe8398ac58f5c4f03ed3f2938214b682bde8e
Akshaj Dave <akshaj.dave@gmail.com>
2020-11-26 17:33:58 +0530

    Add Uploading multiple files

--- src/App.js
+++ src/App.js
@@ -9,36 +9,51 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      file: null,
       reader: new FileReader(),
       chunkSize: 10 * 1024,
-      chunksQueue: []
+      chunksQueue: [],
+      fileArray: []
     }
     
-    this.onSubmitStream = this.onSubmitStream.bind(this);
-    this.onChangeStream = this.onChangeStream.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
     this.sendNext = this.sendNext.bind(this);
     this.upload = this.upload.bind(this);
   }
 
-
-  onSubmitStream(e) {
+  onSubmit(e) {
     e.preventDefault();
 
+    // console.log(this.state.fileArray.length);
+
+    const files = this.state.fileArray;
+    let file;
+
+    for(let i = 0; i < files.length; i++) {
+      file = files.item(i);
+      
+      this.prepare(file, i + 1);
+
+    }
+    
+  }
+
+  prepare(file, fileId) {
+    
     const chunkSize = this.state.chunkSize;
-    const file = this.state.file;
     const chunksQuantity = Math.ceil(file.size / chunkSize);
 
     let chunksQueue = new Array(chunksQuantity).fill().map((_, index) => index).reverse();
+
     this.setState({
         chunksQueue: chunksQueue
     });
     
     // console.log(`Starting upload ${chunksQueue}`);
-    this.sendNext(chunksQueue, chunksQuantity);
+    this.sendNext(chunksQueue, chunksQuantity, fileId);
   }
 
-  sendNext(chunksQueue, chunksQuantity) {
+  sendNext(chunksQueue, chunksQuantity, fileId) {
     
     if(!chunksQueue.length) {
         console.log('All parts uploaded');
@@ -46,7 +61,7 @@ export default class App extends Component {
     }
 
     const chunkSize = this.state.chunkSize;
-    const file = this.state.file;
+    const file = this.state.fileArray[fileId-1];
     const chunkId = chunksQueue.pop();
 
     // console.log(`${chunksQueue}`);
@@ -54,19 +69,19 @@ export default class App extends Component {
     const begin = chunkId * chunkSize;
     const chunk = file.slice(begin, begin + chunkSize);
 
-    this.upload(chunk, chunkId, chunksQuantity)
+    this.upload(chunk, chunkId, chunksQuantity, fileId)
     .then(() => {
-        this.sendNext(chunksQueue, chunksQuantity);
+        this.sendNext(chunksQueue, chunksQuantity, fileId);
     })
     .catch(() => {
         // chunksQueue.push(chunkId);
     });
   }
 
-  upload(chunk, chunkId, chunksQuantity) {
+  upload(chunk, chunkId, chunksQuantity, fileId) {
       // console.log(chunk, chunkId);
       
-      const file = this.state.file;
+      const file = this.state.fileArray[fileId - 1];
 
       /*
       return new Promise((resolve, reject) => {
@@ -77,7 +92,7 @@ export default class App extends Component {
 
       return new Promise((resolve, reject) => {
 
-        const url = 'http://localhost:5000/upload/single-stream';
+        const url = 'http://localhost:5000/upload/chunk';
 
         console.log(chunk.size, chunksQuantity);
 
@@ -86,6 +101,7 @@ export default class App extends Component {
         xhr.open("post", url);
         xhr.setRequestHeader("Content-Type", "application/octet-stream");
         xhr.setRequestHeader("X-Chunk-Id", chunkId);
+        xhr.setRequestHeader("X-File-Id", fileId);
         xhr.setRequestHeader("X-Chunks-Quantity", chunksQuantity);
         // xhr.setRequestHeader("Content-Length", chunk.size);
         xhr.setRequestHeader("X-Content-Name", file.name);
@@ -105,10 +121,10 @@ export default class App extends Component {
   }
 
 
-  onChangeStream(e) {
+  onChange(e) {
     // console.log(e.target.files[0]);
     this.setState({
-      file: e.target.files[0]
+      fileArray: e.target.files
     });
   }
 
@@ -121,9 +137,15 @@ export default class App extends Component {
 
         <section>
           
-          <form onSubmit={this.onSubmitStream} id="stream-single-form">
-          <h1> File Upload - Single (Chunking)</h1>
-            <input type="file" id="single-file-stream" name="single-file-stream" onChange={this.onChangeStream} />
+          <form onSubmit={this.onSubmit} id="stream-single-form">
+          <h1>File Upload - Single (Chunking)</h1>
+            <input type="file" id="single-file-stream" name="single-file-stream" onChange={this.onChange} />
+            <input type="submit" />
+          </form>
+
+          <form onSubmit={this.onSubmit} id="chunk-multiple-form">
+            <h1>File Upload - Multiple (Chunking)</h1>
+            <input type="file" id="multiple-file-chunk" name="multiple-file-chunk" multiple onChange={this.onChange} />
             <input type="submit" />
           </form>
 
